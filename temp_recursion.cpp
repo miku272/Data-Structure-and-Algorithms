bdb524cbdc70042842784cdead73a20ff84fbb04
Naresh Sharma <sharmanaresh272@gmail.com>
2024-05-05 00:45:32 +0530

    .

--- temp_recursion.cpp
+++ temp_recursion.cpp
@@ -2,19 +2,30 @@
 
 void printToNReverse(int n)
 {
-    if (n == 1)
+    if (n < 1)
     {
-        std::cout << n << "\n";
         return;
     }
 
-    std::cout << n << "\n";
+    std::cout << n << " ";
     printToNReverse(n - 1);
+}
+
+void printToN(int n)
+{
+    if (n < 1)
+    {
+        return;
+    }
+
+    printToN(n - 1);
+    std::cout << n << " ";
 }
 
 int main(int argc, char const *argv[])
 {
-    printToNReverse(10);
+    // printToNReverse(10);
+    printToN(10);
 
     return 0;
 }
